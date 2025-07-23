9276dada0fc0bf791415ecf42e6581599efe77a2
Aasritha <aasritha.vemu.019@gmail.com>
2025-07-23 09:50:15 +0530

    First Commit

---
+++ client/src/api/PostRequest.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+const API = axios.create({ baseURL: 'https://mern-mini-demo.onrender.com/' });
+export const getTimelinePosts = (id) => API.get(`/post/${id}/timeline`);
+export const likePost = (id, userId) => API.put(`post/${id}/like_dislike`, { userId: userId })
---
+++ client/src/api/UploadRequest.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+const API = axios.create({ baseURL: 'https://mern-mini-demo.onrender.com/' });
+export const uploadImage = (data) => API.post('/upload/', data);
+export const uploadPost = (data) => API.post('/post', data);
